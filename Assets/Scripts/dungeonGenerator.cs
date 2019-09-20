bed3b72e5fef191e79940a5452e2a793c6774dac
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-09-20 15:54:50 -0400

    tt

--- Assets/Scripts/dungeonGenerator.cs
+++ Assets/Scripts/dungeonGenerator.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class dungeonGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform room;
+    private int num;
+    private List<Rect> rooms=new List<Rect>();
     void Start()
     {
-    
-    
-        
+        for (int i = 0; i < num; i++)
+        {
+            Vector2 point = Random.insideUnitCircle;
+            Vector2 size = Random.insideUnitCircle;
+            point.Scale(new Vector2(10,10));
+            size.Scale(new Vector2(20,20));
+            
+            
+            rooms.Add(new Rect(point,size));
+        }
+
+        Instantiate(room.);
+
+
+
+
+
     }
 
     // Update is called once per frame
