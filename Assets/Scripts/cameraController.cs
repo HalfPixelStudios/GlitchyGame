9a7a510cc2a57baf2b2d67f3ca8ae4abc2fec7ec
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-09-21 14:42:06 -0400

    work on map

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    [Range(0f,10f)] public float camera_speed;
+    public GameObject target;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+        //Smoothly follows a target gamemobject
+        Vector3 target_pos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, target_pos, camera_speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/weaponStat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponStat : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
