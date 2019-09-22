e6adfcc3d7a814a7d2fde7b697132a10bdbd9374
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-09-22 19:22:26 -0400

    not much

--- Assets/Scripts/Enemies/skeletController.cs
+++ Assets/Scripts/Enemies/skeletController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class skeletController : MonoBehaviour {
 
+    public static System.Random rand = new System.Random();
+
+    [Range(0f, 10f)] public float speed;
+
+    private Animator anim;
+    private Rigidbody2D body;
+    private bool isMoving = false; //start off in idle state
+
     void Start() {
         
     }
