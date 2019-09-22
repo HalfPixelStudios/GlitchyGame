a1fac3ff2fd45f1abd57165c86d4161704970d65
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-09-22 16:56:11 -0400

    added more map generation

--- Assets/Scripts/dungeonGenerator.cs
+++ Assets/Scripts/dungeonGenerator.cs
@@ -11,7 +11,7 @@ public class dungeonGenerator : MonoBehaviour
     public GameObject roomPrefab;
     public int num;
     private List<GameObject> rooms=new List<GameObject>();
-    private List<GameObject> mainRooms = new List<GameObject>();
+    
     private bool simDone = false;
     private float mean=0;
     void Start()
@@ -42,6 +42,55 @@ public class dungeonGenerator : MonoBehaviour
         mean /= num;
 
     }
+    //makePaths will remove all unnecessary rooms and add rooms which are the proper size for hallways
+    void makePaths(Dictionary<GameObject, GameObject> parents)
+    {
+        HashSet<GameObject> valid_rooms=new HashSet<GameObject>();
+        
+        
+        foreach (var edge in parents)
+        {
+            BoxCollider2D box1 = edge.Key.GetComponent<BoxCollider2D>();
+            BoxCollider2D box2 = edge.Value.GetComponent<BoxCollider2D>();
+            var dist = box1.Distance(box2);
+            GameObject prev = null;
+            foreach (var r in Physics2D.RaycastAll(box1.transform.position, box1.transform.position-box2.transform.position, dist.distance))
+            {
+                //need to make path in between prev and current box if they are apart
+                if (prev != null)
+                {
+                    //check if they are apart
+                    //otherwise generate a hallway to connect them
+                    
+                    
+                }
+                
+                valid_rooms.Add(r.rigidbody.gameObject);
+                prev = r.rigidbody.gameObject;
+
+            }
+
+            
+
+
+
+        }
+        
+
+        foreach (var vr in rooms)
+        {
+            if (!valid_rooms.Contains(vr))
+            {
+                Destroy(vr);
+            }
+            
+            
+        }
+
+        rooms = valid_rooms.ToList();
+
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -70,8 +119,11 @@ public class dungeonGenerator : MonoBehaviour
         }
         else 
         {
+            
+            List<GameObject> mainRooms = new List<GameObject>();
             foreach (GameObject room in rooms)
             {
+                
                 BoxCollider2D bc2d = room.GetComponent<BoxCollider2D>();
                 if (bc2d.size.x * bc2d.size.y > mean*1.25 )
                 {
@@ -80,15 +132,12 @@ public class dungeonGenerator : MonoBehaviour
                     mainRooms.Add(room);
                     //rooms.Remove(room);
                 }
-                else
-                {
-                    //rooms.Remove(room);
-                    //Destroy(room);
-
-                }
+                
 
                 
-                
+
+
+
             }
             //instead of this preconstruction of graph, Delaunay would be used
             
@@ -96,18 +145,22 @@ public class dungeonGenerator : MonoBehaviour
             Dictionary<GameObject,float> weights=new Dictionary<GameObject, float>();
             Dictionary<GameObject,GameObject> parents=new Dictionary<GameObject, GameObject>();
             mst.Add(rooms[0]);
+            mainRooms.RemoveAt(0);
             foreach (var rm in mainRooms)
             {
                 weights.Add(rm,Single.MaxValue);
                 
             }
 
-            while (mst.Count!=mainRooms.Count)
+            weights.Remove(rooms[0]);
+
+            while (mainRooms.Count!=0)
             {
-                foreach (var obj in weights.Keys)
+                foreach (var obj in mainRooms)
                 {
                     foreach (var node in mst)
                     {
+                        
                         float dist = obj.GetComponent<BoxCollider2D>().Distance(node.GetComponent<BoxCollider2D>())
                             .distance;
                         if (dist < weights[obj])
@@ -116,10 +169,11 @@ public class dungeonGenerator : MonoBehaviour
                             parents[obj] = node;
                         }
                     }
-                    
+
                 }
-                float min=Single.MaxValue;
-                GameObject minNode=null;
+
+                float min = Single.MaxValue;
+                GameObject minNode = null;
                 foreach (var i in weights)
                 {
                     if (i.Value < min)
@@ -129,14 +183,25 @@ public class dungeonGenerator : MonoBehaviour
 
                     }
                 }
+
                 weights.Remove(minNode);
+                mainRooms.Remove(minNode);
                 mst.Add(minNode);
+            }
+            
+            makePaths(parents);
 
-
+            foreach (var r in rooms)
+            {
+                r.GetComponent<TileGenerator>().create = true;
 
 
             }
-            Debug.Log(parents);
+            
+            
+
+            Destroy(this);
+            
             
             
             
