a31b003792831dfa3adabad3bea9fba7e0d4a9aa
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-09-23 19:44:57 -0400

    merge

---
+++ Assets/Scripts/weaponSheath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponSheath : MonoBehaviour { //used to control the weapon from the entity side
+
+    public GameObject equiped_weapon;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
