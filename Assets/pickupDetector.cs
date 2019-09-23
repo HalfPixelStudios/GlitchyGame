709ba1bd7e5906b0f5dd0dd2ca1c31a7dc5691d8
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-09-23 00:02:41 -0400

    bit of work on dropped items

--- Assets/pickupDetector.cs
+++ Assets/pickupDetector.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class pickupDetector : MonoBehaviour {
 
     [Range(0f, 5f)] public float pickup_radius;
+    public GameObject dropped_item_arrow;
+    private GameObject arrow;
 
     void Start() {
         gameObject.GetComponent<CircleCollider2D>().radius = pickup_radius;
@@ -12,13 +14,15 @@ public class pickupDetector : MonoBehaviour {
 
     void Update() { }
 
-
-    private void OnCollisionStay2D(Collision2D other) {
+    private void OnTriggerEnter2D (Collider2D other) {
         //show hovering arrow
+        Debug.Log("Entered collision zone");
+        arrow = Instantiate(dropped_item_arrow, transform.position, transform.rotation);
     }
 
-    private void OnCollisionExit2D(Collision2D other) {
+    private void OnTriggerExit2D(Collider2D other) {
         //kill hovering arrow
+        Destroy(arrow);
     }
 
 }
