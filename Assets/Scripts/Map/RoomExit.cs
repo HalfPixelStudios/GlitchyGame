294eb0c3f084f7cc23c07fca323096dfa452c573
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-10-01 19:06:57 -0400

    entrance list empty for some reason

--- Assets/Scripts/Map/RoomExit.cs
+++ Assets/Scripts/Map/RoomExit.cs
@@ -8,9 +8,7 @@ using SuperTiled2Unity;
 //[ExecuteInEditMode]
 public class RoomExit : MonoBehaviour {
 
-    private GameObject room; //the room that the room exit will generate
-    [SerializeField] private List<GameObject> entrances = new List<GameObject>();
-    [SerializeField] private List<GameObject> exits = new List<GameObject>();
+    public GameObject cur_room;
 
     void Start() {
 
@@ -21,7 +19,7 @@ public class RoomExit : MonoBehaviour {
     }
 
     //TODO: sort rooms by difficulty or theme, and also (possibly) make sure that a room cannot be used twice
-    private void createNewRoom() {
+    private GameObject createNewRoom() {
         //from https://answers.unity.com/questions/1268079/how-to-get-file-names-in-directory.html
         string file_path = "Assets/Resources/Rooms/";
         DirectoryInfo dir = new DirectoryInfo(file_path);
@@ -29,53 +27,25 @@ public class RoomExit : MonoBehaviour {
 
         //pick a random dungeon frame
         string room_path = info[Random.Range(0, info.Length)].Name.Replace(".prefab","");
-        //DONT INIT AS transform.position, transform.rotation!!!!!!
-        room = Instantiate(Resources.Load("Rooms/"+room_path) as GameObject, transform.position, transform.rotation);
-
+        //DONT INIT AS transform.position, transformu.rotation!!!!!!
+        GameObject room = Instantiate(Resources.Load("Rooms/"+room_path) as GameObject, transform.position, transform.rotation);
+        room.AddComponent<RoomInfo>();
+        return room;
     }
 
     private void OnTriggerEnter2D(Collider2D other) { //teleport player to a new room 
+        RoomInfo room_info = cur_room.GetComponent<RoomInfo>();
+
+        if (!room_info.isCleared) { return; } //dont do anything if the room hasnt been cleared yet
+
         //Create the new room
-        createNewRoom();
-        createRoomWarpZones();
+        GameObject new_room = createNewRoom();
 
         //Teleport player to a random room entrance
-        GameObject selected_entrance = exits[Random.Range(0, entrances.Count)];
-        print(other.gameObject.name);
-        other.gameObject.transform.position = selected_entrance.transform.position;
-
-        
-        //Then delete all the entrances
-        foreach (GameObject ent in entrances) { Destroy(ent); }
-        entrances.Clear();
+        other.gameObject.transform.position = new_room.GetComponent<RoomInfo>().getRandomEntrance();
 
         //Destroy current map
-        Destroy(gameObject); //destroy roomExit object
+        Destroy(cur_room); //destroy roomExit object
     }
 
-    private void createRoomWarpZones() { //creates roomExit and roomEntrance objects 
-        GameObject room_zones = room.transform.Find("Grid").gameObject.transform.Find("ROOM_ZONES").gameObject; //Get list of all collision objects in map
-        
-        foreach (Transform child in room_zones.transform) { //get all zone objects
-            CustomProperty property;
-            child.GetComponent<SuperCustomProperties>().TryGetCustomProperty("zone_type", out property); //get custom property obejct
-            GameObject child_ob = child.gameObject;
-
-            if (Equals(property.m_Value,"entrance")) {
-                child_ob.GetComponent<BoxCollider2D>().isTrigger = true;
-
-                entrances.Add(child_ob);
-
-            } else if (Equals(property.m_Value, "exit")) {
-                child_ob.AddComponent<RoomExit>();
-                child_ob.GetComponent<BoxCollider2D>().isTrigger = true;
-
-
-                exits.Add(child_ob);
-
-            }
-            
-        }
-        
-    }
 }
---
+++ Assets/Scripts/Map/RoomInfo.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperTiled2Unity;
+
+public class RoomInfo : MonoBehaviour {
+
+    public bool isCleared = false;
+    [SerializeField] private List<GameObject> entrances = new List<GameObject>();
+    [SerializeField] private List<GameObject> exits = new List<GameObject>();
+
+    void Start() {
+        createRoomWarpZones();
+    }
+
+    void Update() {
+        
+    }
+
+    public void createRoomWarpZones() { //creates roomExit and roomEntrance objects 
+        GameObject room_zones = transform.Find("Grid").gameObject.transform.Find("ROOM_ZONES").gameObject; //Get list of all collision objects in map
+
+        foreach (Transform child in room_zones.transform) { //get all zone objects
+            CustomProperty property;
+            child.GetComponent<SuperCustomProperties>().TryGetCustomProperty("zone_type", out property); //get custom property obejct
+            GameObject child_ob = child.gameObject;
+
+            if (Equals(property.m_Value, "entrance")) {
+                child_ob.GetComponent<BoxCollider2D>().isTrigger = true;
+
+                entrances.Add(child_ob);
+                Debug.Log(entrances.Count, gameObject);
+
+            } else if (Equals(property.m_Value, "exit")) {
+                RoomExit room_exit = child_ob.AddComponent<RoomExit>();
+                room_exit.cur_room = gameObject;
+                child_ob.GetComponent<BoxCollider2D>().isTrigger = true;
+
+
+                exits.Add(child_ob);
+
+            }
+
+        }
+
+    }
+
+    public Vector3 getRandomEntrance() {
+        Debug.Log(entrances.Count, gameObject);
+        GameObject selected_entrance = entrances[Random.Range(0, entrances.Count)];
+        return selected_entrance.transform.position;
+    }
+}
