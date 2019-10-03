748676b3e0839b15b2ce3b76f916643aa4df6fed
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-10-02 22:19:07 -0400

    chest

---
+++ Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour {
+
+    //public bool isOpen = false;
+    private Animator anim;
+
+    void Start() {
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetInteger("State", -1);
+    }
+
+    void Update() {
+        
+    } 
+
+    public void openChest() {
+        anim.SetInteger("State", 0);
+    }
+
+
+}
